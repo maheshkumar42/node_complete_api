1b657d8d2ad78141c7f43e6ef44c14e6f986f838
Mahesh Kumar <maheshkumar@Maheshs-Mac-mini.local>
2023-07-31 09:27:03 -0400

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  //   'https://www.google.ca', // yoursite
+  'http://127.0.0.1:5500',
+  'http://localhost:3500',
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,50 +1,55 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// Custom middleware logger
+app.use(logger);
+
+app.use(credentials);
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+// middleware for cookies
+app.use(cookieParser());
+
+// serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
+app.use('/employees', require('./routes/api/employees'));
+
+// app.use('/)
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 not found' });
+  } else {
+    res.type('txt').send('404 not found');
+  }
 });
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
-
-
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
-
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
